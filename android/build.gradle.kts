ec06b8e6a0a16a55d7f9f51ddfd37aa917065043
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-08-14 14:07:29 +0530

    build: define the android module's configuration information from the version catalog

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,3 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
 }
+
+group = libs.versions.android.group.get()
+version = libs.versions.android.version.get()
