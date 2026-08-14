a24ac3f5e5f08cc3a19468f3912f98cf05ada147
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-08-14 14:11:36 +0530

    build: define the android module's android app package information from the version catalog

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,9 +8,30 @@ plugins {
 group = libs.versions.android.group.get()
 version = libs.versions.android.version.get()
 
-
 kotlin {
     compilerOptions {
         jvmTarget = JvmTarget.JVM_11
+    }
+}
+
+android {
+    namespace = libs.versions.android.group.get()
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.android.group.get()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = libs.versions.android.versionCode.get().toInt()
+        versionName = libs.versions.android.versionName.get()
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+        }
+    }
+    buildFeatures {
+        compose = true
     }
 }
