dca9873c2f4186aa4d1f8b1b600d90145ba14d13
Sudarshan Mhasrup <hello@sudarshanmhasrup.com>
2026-08-31 16:18:25 +0530

    build: configure the android target in the design-system module

--- design-system/build.gradle.kts
+++ design-system/build.gradle.kts
@@ -19,5 +19,11 @@ kotlin {
 
     android {
         namespace = libs.versions.designSystem.group.get()
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
     }
 }
