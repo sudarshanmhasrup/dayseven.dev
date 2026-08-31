412fc1fa34e0e9225787f2f92fccce1e1cf743a7
Sudarshan Mhasrup <hello@sudarshanmhasrup.com>
2026-08-31 17:17:19 +0530

    build: add the desktop JVM target to the compose module

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,5 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
 }
