87b3792930d8fd794a4a779032a7c429a26be884
Sudarshan Mhasrup <hello@sudarshanmhasrup.com>
2026-08-31 15:54:05 +0530

    build: define the design-system module's configuration information from the version catalog

--- design-system/build.gradle.kts
+++ design-system/build.gradle.kts
@@ -1,5 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
 }
+
+group = libs.versions.designSystem.group.get()
+version = libs.versions.designSystem.version.get()
