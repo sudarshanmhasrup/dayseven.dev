8402b4a1703578a91e7353dc7391bd5fa4a0480f
Sudarshan Mhasrup <hello@sudarshanmhasrup.com>
2026-08-31 18:07:34 +0530

    feat: create DesktopApp composable as the entry point for JVM CMP app

---
+++ compose/src/desktopMain/kotlin/dev/dayseven/app/compose/DesktopApp.kt
@@ -0,0 +1,9 @@
+package dev.dayseven.app.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DesktopApp(modifier: Modifier = Modifier) {
+    ComposeApp(modifier = modifier)
+}
